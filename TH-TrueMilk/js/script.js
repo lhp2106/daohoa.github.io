9eae5ef48b91361887edb258e702294bb48fbfb0
minhhoa <daominhhoa93@gmail.com>
2019-12-24 02:38:34 +0700

    uppp

--- TH-TrueMilk/js/script.js
+++ TH-TrueMilk/js/script.js
@@ -2,5 +2,22 @@ $(function(){
 	if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
 		$('body').addClass("isFirefox");
 	}
+
+	$(".btn-dangkyngay").on("click", function(){
+		$("#form-lienhe").show();
+		return false;
+	});
+	$("#form-lienhe .btn-close").on("click", function(){
+		$("#form-lienhe").hide();
+		return false;
+	});
+	$(".btn-dieukhoan").on("click", function(){
+		$("#dieu-khoan").show();
+		return false;
+	});
+	$("#dieu-khoan .btn-close").on("click", function(){
+		$("#dieu-khoan").hide();
+		return false;
+	});
 });
 
