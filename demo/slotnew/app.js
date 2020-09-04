f4276ea5102d7b7ecfd8d269241220825cf0cfa7
minhhoa <daominhhoa93@gmail.com>
2020-09-04 16:10:51 +0700

    uppp

--- demo/slotnew/app.js
+++ demo/slotnew/app.js
@@ -1,3 +1,3 @@
 var APPNAME = 'prod';
-var BASE_URL = 'http://api.demov52.tk';//52.221.206.150
-var BASE_URL_TEST = 'http://api.demov52.tk';//
+var BASE_URL = 'https://api.demov52.tk';//52.221.206.150
+var BASE_URL_TEST = 'https://api.demov52.tk';//
